b840aa3e484c9638ea61ce90d4c68a09dfecd6e3
Vidit Agrawal <viditagrawal56@gmail.com>
2024-08-03 19:59:27 +0530

    feat: made the code more idiomatic, commtting to use clippy

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
-use std::io::{self, Read};
+#![warn(clippy::all, clippy::pedantic)]
 
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use editor::Editor;
+
+mod editor;
 
 fn main () {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {        
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control(){
-                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                }else{
-                    println!("Binary: {0:012b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                }
-        
-                if c == 'q'{    
-                    break;
-                }
-            }
-            Err(err) => println!("Error {}", err),
-        }
-    }
-    disable_raw_mode().unwrap();
+    let editor = Editor::default();
+    editor.run();
 }
